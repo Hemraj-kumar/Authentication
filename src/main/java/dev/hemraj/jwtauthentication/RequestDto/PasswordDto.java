4abb1dea0707d4090bbc203f269febeb83b56047
HEMRAJ KUMAR V S <killerhems004@gmail.com>
2024-09-13 08:22:15 +0530

    updated reset password logic

--- src/main/java/dev/hemraj/jwtauthentication/RequestDto/PasswordDto.java
+++ src/main/java/dev/hemraj/jwtauthentication/RequestDto/PasswordDto.java
@@ -6,7 +6,6 @@ import lombok.Data;
 @AllArgsConstructor
 @Data
 public class PasswordDto {
-    private String confirm_password;
     private String new_password;
-
+    private String confirm_password;
 }
